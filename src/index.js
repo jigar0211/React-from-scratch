1a82bd52ce0310cb095b78ae32cfd000f829425d
jigar <jigar.k@simformsolutions.com>
2023-03-31 13:00:34 +0530

    extra lines removed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
 import App from "./App";
+import ReactDOM from 'react-dom/client';
 
-ReactDOM.render(<App/>, document.querySelector("#root"))
+// ReactDOM.render(<App/>, document.querySelector("#root"))
+// const root = document.getElementById('root');
+// ReactDOM.createRoot(root).render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
